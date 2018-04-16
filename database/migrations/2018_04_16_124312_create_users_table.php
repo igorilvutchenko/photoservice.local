09b2367e20ee693130e5255ae315186a100fa025
Igor Ilvutchenko <corvuscorax84@gmail.com>
2018-04-16 16:28:49 +0300

    fixxed migrations

--- database/migrations/2018_04_16_124312_create_users_table.php
+++ database/migrations/2018_04_16_124312_create_users_table.php
@@ -14,11 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('user_id');
-            $table->string('name', 150);
-            $table->string('email', 150)->unique();
+            $table->increments('id');
+            $table->string('name', 250);
+            $table->string('email', 190)->unique();
             $table->string('password');
-            $table->boolean('is_admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2018_04_16_124445_create_password_resets_table.php
+++ database/migrations/2018_04_16_124445_create_password_resets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateImagesTable extends Migration
+class CreatePasswordResetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateImagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('images', function (Blueprint $table) {
-            $table->increments('image_id');
-            $table->string('image', 100)->unique();
-            $table->timestamps();
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email', 190)->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
@@ -27,6 +27,6 @@ class CreateImagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('images');
+        Schema::dropIfExists('password_resets');
     }
 }
